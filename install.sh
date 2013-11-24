ec1f18c7b9445d620bb61501fbdc1d7788d26b74
Paul Freeman <paul@mojo-projects.com>
2013-11-24 16:12:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 sudo apt-get install -y git
 git clone https://github.com/OddEssay/puppet.git
 sudo apt-get install -y puppet
+sudo apt-get install -y libgemplugin-ruby
+sudo gem install puppet-module
 sudo puppet module install puppetlabs/apt
 sudo puppet module install nodes/php
 
