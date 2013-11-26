6453836ae79849c9116291aaa13f023310ea651a
Paul Freeman <paul@mojo-projects.com>
2013-11-26 10:51:13 +0000

    PHP componentes added

--- www-ip.pp
+++ www-ip.pp
@@ -1,63 +1,97 @@
-package { "apache2": ensure => present, }
-service { "apache2": ensure => "running", require => Package["apache2"], hasrestart=>true }
-user
-{
-	'paul': groups => ["www-data"]
-	
-}
-file
-{
-	'/www':
-		ensure => 'directory',
-		mode => 2660,
-		owner => 'www-data',
-		group => 'www-data',
+class { "php": }
+
+class ipsite {
+  package { "apache2": ensure => present, }
+
+  package { "php5-cli": ensure => present }
+  package { "php-apc": ensure => present, }
+  package { "libssl-dev": ensure => present, }
+  package { "libapache2-mod-php5": ensure => present, }
+
+  package { "php-pear": ensure => present, }
+
+  package { "mongodb":
+    ensure => installed,
+  }
+  service { "mongodb":
+    enable => true,
+    ensure => running,
+    require => Package["mongodb"],
+  }
+  package { 'mongo':
+    ensure   => installed,
+    provider => pecl,
+    require => Package["php-pear"],
+  }
+  file { 'mongo.conf':
+    notify => Service['apache2'],
+    path => '/etc/php5/fpm/conf.d/mongo.ini',
+    content => "extension=mongo.so",
+    mode    => 0640,
+  }
+
+  service { "apache2": ensure => "running", require => Package["apache2"], hasrestart=>true }
+  user
+  {
+    'paul': groups => ["www-data"]
+
+  }
+  file
+  {
+    '/www':
+    ensure => 'directory',
+    mode => 2660,
+    owner => 'www-data',
+    group => 'www-data',
+  }
+
+  file
+  {
+    '/etc/apache2/mods-enabled/headers.load':
+    ensure => link,
+    target => '/etc/apache2/mods-available/headers.load',
+    notify => Service["apache2"],
+    require => Package["apache2"],
+  }
+  file
+  {
+    '/etc/apache2/mods-enabled/rewrite.load':
+    ensure => link,
+    target => '/etc/apache2/mods-available/rewrite.load',
+    notify => Service["apache2"],
+    require => Package["apache2"]
+  }
+  file
+  {
+    'ip':
+    notify	=> Service["apache2"],
+    path    => '/etc/apache2/sites-enabled/ip',
+    ensure  => present,
+    mode    => 0640,
+    content => "NameVirtualHost *:80
+      Listen 80
+      <VirtualHost *:80>
+          ServerAdmin webmaster@paulfreeman.me.uk
+          ServerName pims2
+          DocumentRoot /www/ip/public
+          <Directory />
+                  Options FollowSymLinks
+                  AllowOverride None
+          </Directory>
+          <Directory /www/ip>
+                  Options Indexes FollowSymLinks MultiViews
+                  AllowOverride None
+                  Order allow,deny
+                  allow from all
+          </Directory>
+          <Directory /www/ip/public>
+                  AllowOverride All
+          </Directory>
+          ErrorLog /var/log/apache2/ip_error.log
+          LogLevel warn
+          CustomLog /var/log/apache2/ip_access.log combined
+  </VirtualHost>",
+  }
 }
 
-file
-{
-	'/etc/apache2/mods-enabled/headers.load':
-		ensure => link,
-		target => '/etc/apache2/mods-available/headers.load',
-		notify => Service["apache2"],
-        require => Package["apache2"]
-}
-file
-{
-        '/etc/apache2/mods-enabled/rewrite.load':
-                ensure => link,
-                target => '/etc/apache2/mods-available/rewrite.load',
-	          	notify => Service["apache2"],
-                require => Package["apache2"]
-}
-file
-{
-	'ip':
-		notify	=> Service["apache2"],
-		path    => '/etc/apache2/sites-enabled/ip',
-		ensure  => present,
-		mode    => 0640,
-		content => "NameVirtualHost *:80
-	Listen 80
-	<VirtualHost *:80>
-        ServerAdmin webmaster@paulfreeman.me.uk
-        ServerName pims2
-        DocumentRoot /www/ip/public
-        <Directory />
-                Options FollowSymLinks
-                AllowOverride None
-        </Directory>
-        <Directory /www/ip>
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride None
-                Order allow,deny
-                allow from all
-        </Directory>
-        <Directory /www/ip/public>
-                AllowOverride All
-        </Directory>
-        ErrorLog /var/log/apache2/ip_error.log
-        LogLevel warn
-        CustomLog /var/log/apache2/ip_access.log combined
-</VirtualHost>",
-}
+include ipsite
