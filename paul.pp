6d910e2c5b69d19a69e271e0829a07fcc72fd144
Paul Freeman <paul@mojo-projects.com>
2013-11-26 10:26:43 +0000

    Paul joining right groups

--- paul.pp
+++ paul.pp
@@ -1,11 +1,15 @@
 class paul {
-	user { "paul":
-		ensure=>present,
-		managehome=>true,
-		#Password for first login only, then changed.
-		password => '$1$bjOk/WyV$LKPiyGzUhK.Sb0Ct2MEqM/',
-		shell => '/bin/bash'
-	}
-	package { "vim": ensure => present }
+  group { "admin": ensure => present }
+  group { "www-data": ensure => present }
+  user { "paul":
+    ensure=>present,
+    managehome=>true,
+    #Password for first login only, then changed.
+    password => '$1$bjOk/WyV$LKPiyGzUhK.Sb0Ct2MEqM/',
+    groups => ["paul","www-data","admin"],
+    shell => '/bin/bash',
+    require => [ Group["admin"],Group["www-data"] ]
+  }
+  package { "vim": ensure => present }
 }
 include paul
