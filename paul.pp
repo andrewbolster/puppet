1d273dad9b8d2b49285b65345bc3890e03795e55
Paul Freeman <paul@mojo-projects.com>
2013-11-26 12:07:51 +0000

    Make sure paul group is required for first run success

--- paul.pp
+++ paul.pp
@@ -1,6 +1,7 @@
 class paul {
   group { "admin": ensure => present }
   group { "www-data": ensure => present }
+  group { "paul": ensure => present }
   user { "paul":
     ensure=>present,
     managehome=>true,
@@ -8,7 +9,7 @@ class paul {
     password => '$1$bjOk/WyV$LKPiyGzUhK.Sb0Ct2MEqM/',
     groups => ["paul","www-data","admin"],
     shell => '/bin/bash',
-    require => [ Group["admin"],Group["www-data"] ]
+    require => [ Group["admin"],Group["www-data"],Group["paul"] ]
   }
   package { "vim": ensure => present }
 }
